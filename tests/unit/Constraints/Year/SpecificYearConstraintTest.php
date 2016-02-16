329d56bba592864490c32609c8ba5136d49d722b
Leon Rowland <leon@clearvox.nl>
2016-02-16 15:40:38 +0100

    YearConstraints test

---
+++ tests/unit/Constraints/Year/SpecificYearConstraintTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use Clearvox\DateConstraints\Constraints\Year\SpecificYearConstraint;
+
+class SpecificYearConstraintTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var SpecificYearConstraint
+     */
+    public $constraint;
+
+    public function setUp()
+    {
+        $this->constraint = new SpecificYearConstraint(2015);
+    }
+
+    public function testIsValidWithValidYear()
+    {
+        $this->assertTrue($this->constraint->isValid(new DateTime('2015-01-01 13:00:00')));
+    }
+
+    public function testIsValidWithMoreYears()
+    {
+        $this->constraint
+            ->addYear(2016)
+            ->addYear('2017');
+
+        $this->assertTrue($this->constraint->isValid(new DateTime('2016-01-01')));
+        $this->assertTrue($this->constraint->isValid(new DateTime('2017-01-01')));
+    }
+
+    public function testIsValidWithoutValidYear()
+    {
+        $this->assertFalse($this->constraint->isValid(new DateTime('2016-01-01')));
+    }
+}
